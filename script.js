f5fa15fc38667456927be4610f8286784dfaf208
Ilka Collado <student07@Ilka-Collado.home>
2022-03-24 18:08:41 -0400

    alternating red and blue event listeners online

---
+++ script.js
@@ -0,0 +1,53 @@
+class tttBoard {
+    constructor(boardGrid) {
+        // this.player1 = player1;
+        // this.player2 = player2;
+        this.boardGrid = boardGrid;
+        this.turn = 'red';
+        //add this to constructor for bonus
+        // this.turnTracker = this.turnTracker;
+    }
+
+    currentTurn(element) {
+        if (this.turn === 'red') {
+            element.classList.add('red');
+            this.setBlueTurn();
+        }else{
+            element.classList.add('blue');
+            this.setRedTurn();
+        }
+    }
+
+    getTurn() {
+        return this.turn;
+    }
+
+    setRedTurn() {
+        return this.turn = 'red';
+    }
+
+    setBlueTurn() {
+        return this.turn = 'blue';
+    }
+
+    resetGame() {
+
+    }
+
+    addEventListeners(){
+        for (let i=0; i<this.boardGrid.length; i++) {
+            this.boardGrid[i].addEventListener('click', () => {
+                if (this.boardGrid[i].classList.contains('red') || (this.boardGrid[i].classList.contains('blue'))) {
+                    this.boardGrid[i].removeEventListener('click', arguments.callee);
+                }else{
+                    this.currentTurn(this.boardGrid[i])
+                }
+            })
+        }
+    }
+}
+
+const squares = document.querySelectorAll('.grid-square');
+
+board = new tttBoard(squares);
+board.addEventListeners();
